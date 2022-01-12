c157e87483367ad339dbb9292b262e40a31f781f
Roshan Padaki <roshanpadaki@gmail.com>
2022-01-12 01:18:20 +0000

    build of commit 0b3224d916f9112c2b0f45ce5738ff8fa0e86ec9 from rpadaki/bio

--- index-c79c0ac8881c13a.js
+++ index-c79c0ac8881c13a.js
@@ -263,7 +263,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-37ad4befe2ab2758_bg.wasm', import.meta.url);
+        input = new URL('index-c79c0ac8881c13a_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
